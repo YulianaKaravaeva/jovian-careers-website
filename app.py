74d883a399db44c5cc0afec52a51c533c5939085
YulianaKaravaeva <93388991+YulianaKaravaeva@users.noreply.github.com>
2023-12-11 17:45:52 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -1,14 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 # Создание flask app
 app = Flask(__name__)
 
+# Симулятор базы данных
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bishkek, Kyrgyzstan',
+    'salary': '15 000 $'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': '10 000 $'
+  },
+  {
+    'id': 3,
+    'title': 'Frontand Engineer',
+    'location': 'Remout'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'USA',
+    'salary': '120 000 $'
+  }
+]
+
 # Регистрация маршрута ("/") - домашняя, главная страница
 @app.route("/")
+
 # Функция, которая будет выполняться при запросе к домашней страниц
-def hello_world():
+def hello_jovian():
   # Визуализировать шаблон
-  return render_template("home.html")
+  # Отправить в шаблон параметр с данными
+  return render_template("home.html",
+                        jobs=JOBS,
+                        company_name="Jovian")
+
+@app.route("/api/jobs")
+
+# Использование API для получения данных (отображение информации в формате json)
+def list_jobs():
+
+  # Преобразование данных в формат json
+  return jsonify(JOBS)
 
 # Проверка, что запускаем именно это приложение app.py
 if __name__ == "__main__":
